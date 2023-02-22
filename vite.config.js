d9f34acc7b56deb95492d99afce0177a0e63835c
Ling-siou <liao.siou@gmail.com>
2023-02-22 18:07:19 +0800

    set base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vite-deploy-sample',
+  base: '/vite-deploy-test',
   plugins: [
     vue(),
     eslintPlugin({
