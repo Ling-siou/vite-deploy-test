d67b1fff323b1fe4206dd6099db14344cd8226d0
Ling-siou <liao.siou@gmail.com>
2023-02-22 18:01:16 +0800

    all files

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+/* eslint-env node */
+// require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src'],
+        ],
+        extensions: ['.js', '.vue'],
+      },
+    },
+    'import/core-modules': [
+      'vite',
+      '@vitejs/plugin-vue',
+    ],
+  },
+};
---
+++ src/views/AboutView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="about">
+    <div>
+      <p>{{ content }}</p>
+      <QuillEditor v-model:content="content"></QuillEditor>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script>
+import { QuillEditor } from '@vueup/vue-quill';
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
+const url = import.meta.env.VITE_URL;
+
+export default {
+  data() {
+    return {
+      content: '',
+    };
+  },
+  created() {
+  },
+  components: {
+    QuillEditor,
+  },
+  methods: {
+    getTestData() {
+      this.$http.get(`${url}comments`).then((response) => {
+        console.dir(response.data);
+      });
+    },
+  },
+};
+</script>
